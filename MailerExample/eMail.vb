5bb7053ca932292ae317f7451daaf77d4a7fd764
Benjamin <jb.ballesteros.mtz@outlook.com>
2017-12-11 21:33:24 -0800

    Added data model

---
+++ MailerExample/eMail.vb
@@ -0,0 +1,7 @@
+﻿Public Class eMail
+    Property Subject As String
+    Property Body As String
+    Property Recipients As List(Of String)
+    Property AttachmentPath As String
+    Property DisplayName As String
+End Class
